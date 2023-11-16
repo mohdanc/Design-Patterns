cebaccd2a7e1ab7e9b0535cc1b06ae912677c6a6
Muhammad Ans <m.ans@devsinc.com>
2023-11-16 22:00:44 +0500

    Fix: docstring

--- abstract_factory_method.py
+++ abstract_factory_method.py
@@ -3,60 +3,76 @@
     for creating families of related or dependent objects without specifying their concrete classes.
     This pattern involves multiple interfaces, each representing a family of related products.
 '''
-
 from abc import ABC, abstractmethod
 
 # Abstract Product Interfaces
 class Animal(ABC):
+    """Abstract product interface for different types of animals."""
     @abstractmethod
     def speak(self):
-        pass
+        '''Speak method'''
 
 class Habitat(ABC):
+    """Abstract product interface for different types of habitats."""
     @abstractmethod
     def description(self):
-        pass
+        '''Description method'''
 
 # Concrete Product Classes
 class Dog(Animal):
+    """Concrete product representing a Dog."""
     def speak(self):
+        """Return the sound a dog makes."""
         return "Woof!"
 
 class Cat(Animal):
+    """Concrete product representing a Cat."""
     def speak(self):
+        """Return the sound a cat makes."""
         return "Meow!"
 
 class Forest(Habitat):
+    """Concrete product representing a Forest habitat."""
     def description(self):
+        """Return a description of the forest habitat."""
         return "A forest habitat."
 
 class House(Habitat):
+    """Concrete product representing a House habitat."""
     def description(self):
+        """Return a description of the house habitat."""
         return "A house habitat."
 
 # Abstract Factory Interface
 class AbstractFactory(ABC):
+    """Abstract factory interface for creating families of products."""
     @abstractmethod
     def create_animal(self) -> Animal:
-        pass
+        '''Create animal'''
 
     @abstractmethod
     def create_habitat(self) -> Habitat:
-        pass
+        '''Create habitat'''
 
 # Concrete Factory Classes
 class ForestFactory(AbstractFactory):
+    """Concrete factory creating a family of products for a forest habitat."""
     def create_animal(self) -> Animal:
+        """Create and return a Dog."""
         return Dog()
 
     def create_habitat(self) -> Habitat:
+        """Create and return a Forest habitat."""
         return Forest()
 
 class HouseFactory(AbstractFactory):
+    """Concrete factory creating a family of products for a house habitat."""
     def create_animal(self) -> Animal:
+        """Create and return a Cat."""
         return Cat()
 
     def create_habitat(self) -> Habitat:
+        """Create and return a House habitat."""
         return House()
 
 # Client Code
